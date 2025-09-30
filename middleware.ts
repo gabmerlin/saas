bf64d90b2382ac34b6bd1116d091d5ffe3fad9a7
gabmerlin <gabriel.merlin.contact@gmail.com>
2025-09-30 12:22:29 +0200

    SAAS

--- middleware.ts
+++ middleware.ts
@@ -264,16 +264,3 @@ function extractSubdomain(host: string, rootDomain: string): string | null {
   
   return null
 }
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
